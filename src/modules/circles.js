d4eca7a64efc3bdb8275f7bfb0e6d83894e3fcdd
rsmelo <rodrigosm87@gmail.com>
2016-04-04 02:40:54 -0300

    refactoring circles

--- src/modules/circles.js
+++ src/modules/circles.js
@@ -1,11 +1,11 @@
 "use strict";
 let canvas,
-    ctx,   
-    tick = 0,
-    colorHue = 10,
-    angulo = 0,
-    numLinhas = 1,
-    bolaPorLinha = 6;
+ctx,   
+tick = 0,
+colorHue = 10,
+angle = 0,
+linesNumber = 1,
+ballsPerline = 6;
 
 
 var init = function() {
@@ -22,52 +22,54 @@ var loop = function () {
   requestAnimationFrame( loop );  
   draw();
 };
- 
-var drawCircle = function (posX, posY, raio){
+
+var drawCircle = function (ctx, posX, posY, radius){
     if (colorHue > 360) {
         colorHue = 0;
     } else {
-        colorHue += 1;    
+        colorHue++;
     }
 
     ctx.beginPath();
     ctx.fillStyle = "hsla(" + colorHue + ", 100%, 50%, 1 )";
-    ctx.arc(posX, posY, raio, 0, Math.PI * 2, true);      
+    ctx.arc(posX, posY, radius, 0, Math.PI * 2, true);      
     ctx.closePath();
     ctx.fill();
 };
 
 var draw = function () {    
-    let raio = 40,
-        centerX = canvas.width / 2,
-        centerY = canvas.height / 2;
+    let radius = 40,
+    centerX = canvas.width / 2,
+    centerY = canvas.height / 2;
 
     ctx.beginPath();    
     ctx.fillStyle = "#000";
     ctx.fillRect( 0, 0, canvas.width, canvas.height);         
     ctx.closePath();
 
-    drawCircle(centerX, centerY, raio);
+    drawCircle(ctx, centerX, centerY, radius);
 
-    let fracaoAngulo = 360 / bolaPorLinha;
-    for (let numAngulo = 0; numAngulo <= 360; numAngulo += fracaoAngulo){
+    let angleStep = 360 / ballsPerline;
+    for (let currentAngle = 0; currentAngle <= 360; currentAngle += angleStep){
 
-        for (let iLinha = 1; iLinha <= numLinhas; iLinha++){
+        for (let iLinha = 1; iLinha <= linesNumber; iLinha++){
             drawCircle(
-                centerX - (raio * 2 * iLinha) * Math.cos(numAngulo * Math.PI / 180) ,
-                centerY - (raio * 2 * iLinha)  * Math.sin(numAngulo * Math.PI / 180), 
-                raio
-            );
+                ctx,
+                centerX - (radius * 2 * iLinha) * Math.cos(currentAngle * Math.PI / 180) ,
+                centerY - (radius * 2 * iLinha)  * Math.sin(currentAngle * Math.PI / 180), 
+                radius
+                );
 
         }   
     }
-        
-    angulo += 10;
-    if (angulo >= 360){
-        angulo = 0;
-        if (numLinhas < 15 )
-            numLinhas++;        
-        bolaPorLinha += 3;
+
+    angle += 10;
+    if (angle >= 360){
+        angle = 0;
+        if (linesNumber < 15 ){
+            linesNumber++;        
+        }        
+        ballsPerline += 3;
     }
 
     ctx.save();     
