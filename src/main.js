462b1ae9a26be05c17da4cb5c839dadffcf19df5
rsmelo <rodrigosm87@gmail.com>
2016-04-04 02:41:09 -0300

    migrating triangles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import "./css/main.css";
-import circle from "./modules/circles.js";
-circle.init();
+//import circle from "./modules/circles.js";
+import triangles from "./modules/triangles.js";
+
+triangles.init();
