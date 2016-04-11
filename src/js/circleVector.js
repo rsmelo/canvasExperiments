fba439186328dbcda5b0789a20add584c2ce4033
Rodrigo Melo <rodrigosm@labtrans.ufsc.br>
2016-04-11 18:39:30 -0300

    circle vcetor, created lines, started to create points to each line

--- src/js/circleVector.js
+++ src/js/circleVector.js
@@ -1,10 +1,15 @@
 import "../css/main.css";
 import "../template/circleVector.html";
-let lines = 2;
+let slices = 4;
+const ballRadius = 6;
+const balls = [];
+let distance = 1;
+let positiveIncrement = true;
+let angulo = 0;
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
-const radius = 100;
+const radius = 200;
 
 function init() {
     const body = document.getElementsByTagName("body")[0];
@@ -23,31 +28,62 @@ function drawCircle(context, posX, posY, radius) {
     context.stroke();
 }
 
+function drawLines(ctx) {
+    for (let iLines = 0; iLines < lines; iLines++) {
+        ctx.beginPath();
+        ctx.moveTo(centerX - radius * Math.cos(360 * Math.PI / 180), centerY - radius * Math.sin(360 * Math.PI / 180));
+        ctx.lineTo(centerX - radius * Math.cos(180 * Math.PI / 180), centerY - radius * Math.sin(180 * Math.PI / 180));
+        ctx.stroke();
+    }
+}
+
+function drawPoint(context, centerX, centerY) {
+    context.beginPath();
+    context.fillStyle = "#f00";
+    context.arc(distance + centerX - radius * Math.cos(0 * Math.PI / 180),centerY - radius * Math.sin(0 * Math.PI / 180), ballRadius, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+
+    if (positiveIncrement) {
+        distance += 2;
+    } else {
+        distance -= 2;
+    }
+
+    if (distance >= radius * 2) {
+        positiveIncrement = false;
+    } else if (distance <= 0) {
+        positiveIncrement = true;
+        if (slices < 40) {
+            slices += 2;
+        } else {
+            slices = 4;
+        }
+    }
+}
+
 function draw() {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.closePath();
+
     drawCircle(ctx, centerX, centerY, radius);
 
+    const delta = 360 / slices;
+    let currentVariance = 0;
+    for (let iLines = 0; iLines < (slices / 2); iLines++) {
+        ctx.beginPath();
+        ctx.moveTo(centerX - radius * Math.cos(currentVariance * Math.PI / 180), centerY - radius * Math.sin(currentVariance * Math.PI / 180));
+        ctx.lineTo(centerX - radius * Math.cos((currentVariance + 180) * Math.PI / 180), centerY - radius * Math.sin((currentVariance + 180) * Math.PI / 180));
+        ctx.stroke();
+        currentVariance += delta;
+    }
 
-    // for (let i =1; i <= lines; i++) {
-    //     let angulo1 = 360 / lines;
-    //     let angulo2 = angulo1 + 180;
-    //     ctx.beginPath();
-    //     ctx.moveTo(centerX - radius * Math.cos(angulo1 * Math.PI / 180), centerY - radius * Math.sin(angulo1 * Math.PI / 180));
-    //     ctx.lineTo(centerX - radius * Math.cos(angulo2 * Math.PI / 180), centerY - radius * Math.sin(180 * Math.PI / 180));
-    //     ctx.stroke();
-    // }
-
-    ctx.beginPath();
-    ctx.moveTo(centerX - radius * Math.cos(360 * Math.PI / 180), centerY - radius * Math.sin(360 * Math.PI / 180));
-    ctx.lineTo(centerX - radius * Math.cos(180 * Math.PI / 180), centerY - radius * Math.sin(180 * Math.PI / 180));
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(centerX - radius * Math.cos(90 * Math.PI / 180), centerY - radius * Math.sin(90 * Math.PI / 180));
-    ctx.lineTo(centerX - radius * Math.cos(270 * Math.PI / 180), centerY - radius * Math.sin(270 * Math.PI / 180));
-    ctx.stroke();
+    drawPoint(ctx, centerX, centerY);
 }
 
 function loop() {
